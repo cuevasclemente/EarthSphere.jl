a1ca26fe6228c5d837354a5eef96d71637908c85
Clemente <cuevasclemente@gmail.com>
2015-06-01 18:07:05 -0400

    Type annotations everywhere

--- src/EarthSphere.jl
+++ src/EarthSphere.jl
@@ -25,8 +25,8 @@ type Sphere_LatLong_Coord <: LatLong_Coord
 	radius::Float64 # 6371000 Meters for earth
 end
 function Earth_LatLong_Coord(latitude::Degree,longitude::Degree,bearing::String)
-	latnumber = degree_to_decimal_degree(latitude)
-	longnumber = degree_to_decimal_degree(longitude)
+	latnumber::Float64 = degree_to_decimal_degree(latitude)
+	longnumber::Float64 = degree_to_decimal_degree(longitude)
 	Sphere_LatLong_Coord(latnumber,longnumber,bearing,6371)
 end
 export Earth_LatLong_Coord
@@ -95,7 +95,7 @@ end
 
 function sign_lat_long(latlong::LatLong_Coord)
 	"Signs a latlong coord using the general sign_lat_long"
-	(newlat,newlong) = sign_lat_long(latlong.latitude,latlong.longitude,latlong.bearing)
+	(newlat::Float64,newlong::Float64) = sign_lat_long(latlong.latitude,latlong.longitude,latlong.bearing)
 end
 
 
@@ -108,8 +108,8 @@ end
 
 function convert(Spherical_Coord,latlong::LatLong_Coord)
 	(signedlat,signedlong) = sign_lat_long(latlong.latitude,latlong.longitude,latlong.bearing)
-	phi = to_radians(signedlat)
-	theta =  to_radians(signedlong)
+	phi::Float64 = to_radians(signedlat)
+	theta::Float64 =  to_radians(signedlong)
 	return Spherical_Coord(latlong.radius,theta,phi)
 end
 export convert
@@ -129,8 +129,8 @@ end
 function to_radians(degree::Real)
 	"Takes a degree, returns radians"
 	_sign = sign(degree)
-	reduced_degree = mod(abs(degree),360)
-	radians = reduced_degree*(pi/180)
+	reduced_degree::Float64 = mod(abs(degree),360)
+	radians::Float64 = reduced_degree*(pi/180)
 	return radians*_sign
 end
 
@@ -141,28 +141,28 @@ function get_distance(c1::Spherical_Coord,c2::Spherical_Coord)
 	end
 	#We're using the Vincenty formula, because it appears to be very computationally and algorithmically reasonable.
 	#=central_angle = spherical_law_of_cosines(c1,c2)=#
-	central_angle = vincenty(c1,c2)
+	central_angle::Float64 = vincenty(c1,c2)
 	#=central_angle = haversine(c1,c2)=#
 	#=central_angle = alternative_haversine(c1,c2)=#
-	distance = c1.radius*central_angle
+	distance::Float64 = c1.radius*central_angle
 
 	return distance
 end
 function get_distance(c1::Sphere_LatLong_Coord,c2::Sphere_LatLong_Coord)
 	c1s = convert(Spherical_Coord,c1)
 	c2s = convert(Spherical_Coord,c2)
-	return get_distance(c1s,c2s)
+	return get_distance(c1s,c2s)::Float64
 end
 
 export get_distance
 
 function vincenty(c1::Spherical_Coord,c2::Spherical_Coord)
 	#An implementation of the Vincenty formula: http://en.wikipedia.org/wiki/Great-circle_distance
-	longdiff = c1.theta-c2.theta
+	longdiff::Float64 = c1.theta-c2.theta
 	#We could take absolute value of long (the difference of longitudes) but we only ever take the cosine of it, so we can just leave it (cosine is even, sign doesn't matter)
-	vin_numerator = ( (cos(c2.phi)*sin(longdiff))^2 + ( cos(c1.phi)*sin(c2.phi) - sin(c1.phi)*cos(c2.phi)*cos(longdiff))^2 )^(1/2) 
-	vin_denominator = sin(c1.phi)*sin(c2.phi)+cos(c1.phi)*cos(c2.phi)*cos(longdiff)
-	central_angle = atan2(vin_numerator,vin_denominator)
+	vin_numerator::Float64 = ( (cos(c2.phi)*sin(longdiff))^2 + ( cos(c1.phi)*sin(c2.phi) - sin(c1.phi)*cos(c2.phi)*cos(longdiff))^2 )^(1/2) 
+	vin_denominator::Float64 = sin(c1.phi)*sin(c2.phi)+cos(c1.phi)*cos(c2.phi)*cos(longdiff)
+	central_angle::Float64 = atan2(vin_numerator,vin_denominator)
 	return central_angle
 end
 vincenty(c1,c2) = vincenty(convert(Spherical_Coord,c1),convert(Spherical_Coord,c2))
@@ -170,26 +170,24 @@ spherical_law_of_cosines(c1,c2) = spherical_law_of_cosines(convert(Spherical_Coo
 function spherical_law_of_cosines(c1::Spherical_Coord,c2::Spherical_Coord)
 	#Implements Spherical Law of cosines, neither vincenty nor this are getting me close to google maps
 	#UPDATE!: It's okay, I just accidentally had latitude for longitude and longitude for latitude, not really a big deal except it broke everything
-	longdiff = abs(c1.theta-c2.theta)
-	central_angle = acos(sin(c1.phi)*sin(c2.phi)+cos(c1.phi)*cos(c2.phi)*cos(longdiff))
+	longdiff::Float64 = abs(c1.theta-c2.theta)
+	central_angle::Float64 = acos(sin(c1.phi)*sin(c2.phi)+cos(c1.phi)*cos(c2.phi)*cos(longdiff))
 	return central_angle
 end
 function haversine(c1::Spherical_Coord,c2::Spherical_Coord)
-	longdiff = c1.theta-c2.theta
-	latdiff = c1.phi-c2.phi
-	a = (sin(latdiff/2))^2 + cos(c1.phi)*cos(c2.phi)*(sin(longdiff/2))^2
-	central_angle = 2*atan2(sqrt(a),sqrt(1-a))
+	longdiff::Float64 = c1.theta-c2.theta
+	latdiff::Float64 = c1.phi-c2.phi
+	a::Float64 = (sin(latdiff/2))^2 + cos(c1.phi)*cos(c2.phi)*(sin(longdiff/2))^2
+	central_angle::Float64 = 2*atan2(sqrt(a),sqrt(1-a))
 	return central_angle
 end
 function alternative_haversine(c1::Spherical_Coord,c2::Spherical_Coord)
 	#This is another formulation of the haversine formula from wikipedia
 	
-	latdiff = abs(c1.phi-c2.phi)
-	println(latdiff)
-	longdiff = abs(c1.theta-c2.theta)
-	println(longdiff)
-	argument = sqrt(sin((latdiff)/2)*sin((latdiff)/2) - cos(c1.phi)*cos(c2.phi)*sin((longdiff)/2)*sin((longdiff)/2))
-	central_angle = 2*asin(argument)
+	latdiff::Float64 = abs(c1.phi-c2.phi)
+	longdiff::Float64 = abs(c1.theta-c2.theta)
+	argument::Float64 = sqrt(sin((latdiff)/2)*sin((latdiff)/2) - cos(c1.phi)*cos(c2.phi)*sin((longdiff)/2)*sin((longdiff)/2))
+	central_angle::Float64 = 2*asin(argument)
 	return central_angle
 end
 
